6cb753ba1befa25d5fab4d7ce33fdc856f263c8e
resZulfikar <resazulf@gmail.com>
2020-04-10 16:49:13 +0700

    Second Commit

--- Assets/ballscript.cs
+++ Assets/ballscript.cs
@@ -7,19 +7,35 @@ public class ballscript : MonoBehaviour
 
     public int speed = 21;
 
+    //object reference
+    public Rigidbody2D objek1;
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         //Debug.Log("Hello Wordl");
         //komponen bola agar memantul
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed; 
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed; 
 
+        //karakteristik objek reference
+        objek1.velocity = new Vector2(-1, -1) * speed;
+
+        anim.SetBool("IsMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+       if(objek1.velocity.x > 0)
+        {
+            objek1.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            objek1.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
